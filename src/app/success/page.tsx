f6e6d37a1294bb35b44886215648845fb74e7c04
alinewprogrammer <alihassan182006@gmail.com>
2025-11-12 09:06:35 +0300

    update: change Stripe API version to 2025-08-27.basil

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -14,7 +14,7 @@ import { Logo } from "@/components/Logo";
 import { FadeIn } from "@/components/FadeIn";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-07-30.basil",
+  apiVersion: "2025-08-27.basil",
 });
 
 export const metadata: Metadata = {
